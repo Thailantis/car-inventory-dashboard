b14079ee4af58572e025405bb7782f977c38809b
Thailantis <128191487+Thailantis@users.noreply.github.com>
2023-06-03 12:37:28 -0400

    Update SaveCar.tsx

--- src/page/SaveCar.tsx
+++ src/page/SaveCar.tsx
@@ -20,3 +20,20 @@ interface CarDetailProps {
   user: User;
   onSaveCar: (carL Car) => void;
 }
+
+const CarDetail: React.FC<CarDetailProps> = ({ car, user, onSaveCar }) => {
+  const handleSaveCar = () => {
+    onSaveCar(car);
+  }
+  
+  return (
+    <div>
+        <h2>{car.make} {car.model}</h2>
+        <p>Color: {car.color}</p>
+        <p>Year: {car.year}</p>
+        <button onClick={handleSaveCar}>Save Car</button>
+    </div>
+  );
+};
+
+export default SaveCar;
